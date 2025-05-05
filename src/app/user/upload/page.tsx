b6ed1805d02be28f18c6bb4b58fae86c87fc9c84
Khayden Roberts <122504123+Hoochymoochy@users.noreply.github.com>
2025-05-04 21:16:52 -0600

    added photo route

---
+++ src/app/user/upload/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Play, Users, Globe, Share, Heart, MessageSquare } from "lucide-react";
+import Image from "next/image";
+import PhotoUpload from "@/app/components/photouploade";
+
+export default function BraindanceUserStream() {
+  return (
+    <div className="min-h-screen bg-[#0a0014] text-white font-sans">
+      <PhotoUpload eventId="" userId="" />
+    </div>
+  );
+}
